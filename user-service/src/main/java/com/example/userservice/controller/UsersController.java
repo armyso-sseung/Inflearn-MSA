414b8feaca35714f7bd8f67a8a13590209ee273a
정승현 <armyso9506@plateer.com>
2024-06-04 18:39:15 +0900

    [ MSA ] Feature: User Service

--- user-service/src/main/java/com/example/userservice/controller/UsersController.java
+++ user-service/src/main/java/com/example/userservice/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.example.userservice.controller;
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.Greeting;
@@ -14,9 +15,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UsersController {
     private final Environment env;
@@ -48,6 +55,14 @@ public class UsersController {
     }
 
     @GetMapping("/users")
-    public void getUserList() {
+    public ResponseEntity<List<ResponseUser>> getUserList() {
+        Iterable<UserEntity> userList = userService.getUserByAll();
+
+        ModelMapper mapper = new ModelMapper();
+        List<ResponseUser> users = StreamSupport.stream(userList.spliterator(), false)
+                .map(user -> mapper.map(user, ResponseUser.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(users);
     }
 }
